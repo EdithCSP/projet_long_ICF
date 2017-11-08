87673c2d9a35cbe3c03678404357655c2bfc6b56
EdithCSP <31658760+EdithCSP@users.noreply.github.com>
2017-11-08 19:46:40 +0100

    Update script.py

--- src/script.py
+++ src/script.py
@@ -5,20 +5,25 @@
 import re
 
 
-fa_file = open("/home/cspe/M2_BI/Projet_long/fwdfastafiles/Galaxy25-[FASTA_hypo_Zbtb24mut_genes].fasta", "r")
+fa_file = open("../../fwdfastafiles/Galaxy25-[FASTA_hypo_Zbtb24mut_genes].fasta", "r")
 liste_fa = fa_file.readlines()
-filout =  open("/home/cspe/M2_BI/Projet_long/fwdfastafiles/test.fasta", "w")
+filout =  open("../results/test.fasta", "w")
 
 new_seq = '>'
 flag = False
 for line in liste_fa:
-    if re.search(new_seq, line):
-        flag = True
-        filout.write(line)
-    elif(flag == True):
-        line = line.replace("\n","")
-        filout.write(line)
-    filout.write("\n")
+	if re.search(new_seq, line):
+		flag = True
+		#filout.write(line)
+		print(line)
+		li = ""
+	elif(flag == True):
+		line = line.replace("\n","")
+		#filout.write(line)
+		li = li + line
+	print(li)
+	
+	filout.write("\n")
 filout.close()
 fa_file.close()
     

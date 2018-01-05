06a1f8db4ba5672efe7f742197a12b49db445996
m2bi EDITH CHAN <echan@lk-281b-wle.ens.sdv.univ-paris-diderot.fr>
2018-01-05 19:55:06 +0100

    rajout fonction find_GC_in_motif + result

--- src/script.py
+++ src/script.py
@@ -360,6 +360,40 @@ def create_dico_knwon_unknown_motif():
 	return dico_motif_knwon_unknown_motif
 
 
+def find_GC_in_motif():
+	'''
+		Cette fonction permet de trouver les motifs qui contienent un GC parmis 
+		les motifs connus et de novo
+		INPUT:
+			- liste motifs connus ou non
+		OUTPUT:
+			- liste motifs connus ou non avec GC ou non
+	'''
+	filin1 = open("../results/known_motif.txt","r")
+	filin2 = open("../results/unknown_motif.txt","r")
+	known_motif = filin1.readlines()
+	unknown_motif = filin2.readlines()
+	filin1.close()
+	filin2.close()
+	filout1 = open("../results/GC_plus_known_motif.txt","w")
+	filout2 = open("../results/GC_moins_known_motif.txt","w")
+	for elt in known_motif: 
+		if("GC" in elt):
+			filout1.write(elt)
+		else:
+			filout2.write(elt)
+	filout1.close()
+	filout2.close()
+	filout1 = open("../results/GC_plus_unknown_motif.txt","w")
+	filout2 = open("../results/GC_moins_unknown_motif.txt","w")
+	for elt in unknown_motif: 
+		if("GC" in elt):
+			filout1.write(elt)
+		else:
+			filout2.write(elt)
+	filout1.close()
+	filout2.close()
+
 if __name__ == '__main__': 
 	os.system("export PATH=$HOME/meme/bin:$PATH")
 	dico_all_seq = create_dico_seq_concate_with_fasta("/home/sdv/m2bi/echan/M2BI/Projet_long/fwdfastafiles/Galaxy25-[FASTA_hypo_Zbtb24mut_genes].fasta")
@@ -376,7 +410,6 @@ if __name__ == '__main__':
 	launch_find_known_motif()
 	dico_knwon_unknown_motif = create_dico_knwon_unknown_motif()
 	##print(dico_knwon_unknown_motif)
-	
-
-
-
+	find_GC_in_motif()
+	# subprosses.call
+# recherche GC
